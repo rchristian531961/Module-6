8140f0220bed1f44603bbf6b7e5ecf78fc74dceb
Ray Christian <rchristian531961@yahoo.com>
2023-07-07 11:12:59 -0500

    Completed Module 6

--- lab3/src/App.jsx
+++ lab3/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import EmojiDisplay from './Emojis.jsx/EmojiDisplay'
-
+import {Route,Routes} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,7 +30,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <EmojiDisplay/>
+      
+      <Routes>  
+      <Route path='/Emoji' element={<EmojiDisplay/>}/>
+      </Routes>
+      
+      
     </>
   )
 }
